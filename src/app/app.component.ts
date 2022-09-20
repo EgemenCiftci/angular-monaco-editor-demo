4bf14242bdc5058983ec9f87463aa4af6b66e806
EgemenCiftci <egemenciftci@gmail.com>
2022-09-20 16:10:41 +0200

    9 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,43 @@
-import { Component, ViewChild } from "@angular/core";
+import { Component, ViewChild } from '@angular/core';
 import { filter, take } from 'rxjs/operators';
 import {
   MonacoEditorComponent,
   MonacoEditorConstructionOptions,
   MonacoEditorLoaderService,
-  MonacoStandaloneCodeEditor
-} from "@materia-ui/ngx-monaco-editor";
+  MonacoStandaloneCodeEditor,
+} from '@materia-ui/ngx-monaco-editor';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   @ViewChild(MonacoEditorComponent, { static: false })
   monacoComponent: MonacoEditorComponent;
   editorOptions: MonacoEditorConstructionOptions = {
-    language: "xml",
+    language: 'xml',
     roundedSelection: true,
-    autoIndent: "full"
+    autoIndent: 'full',
   };
   code = this.getCode();
 
   constructor(private monacoLoaderService: MonacoEditorLoaderService) {
     this.monacoLoaderService.isMonacoLoaded$
       .pipe(
-        filter(isLoaded => isLoaded),
+        filter((isLoaded) => isLoaded),
         take(1)
       )
-      .subscribe(() => {
-
-      });
+      .subscribe(() => {});
   }
 
-  editorInit(editor: MonacoStandaloneCodeEditor) {
-
-  }
+  editorInit(editor: MonacoStandaloneCodeEditor) {}
 
   getCode() {
     return (
       // tslint:disable-next-line: max-line-length
       '<html><!-- // !!! Tokens can be inspected using F1 > Developer: Inspect Tokens !!! -->\n<head>\n	<!-- HTML comment -->\n	<style type="text/css">\n		/* CSS comment */\n	</style>\n	<script type="javascript">\n		// JavaScript comment\n	</' +
-      "script>\n</head>\n<body></body>\n</html>"
+      'script>\n</head>\n<body></body>\n</html>'
     );
   }
 }
